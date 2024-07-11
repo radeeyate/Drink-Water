5f44ee0e529e0dfb42951df8ecdac62e197fb89a
Andrew <me@radi8.dev>
2024-07-11 13:10:35 -0600

    initial commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/ctcpip/notifize"
+	"github.com/go-co-op/gocron/v2"
+	"time"
+	"fmt"
+	"os"
+)
+
+func main() {
+	s, _ := gocron.NewScheduler()
+
+	j, _ := s.NewJob(
+		gocron.DurationJob(1*time.Second),
+		gocron.NewTask(func() { notify() }),
+	)
+
+	fmt.Println(j.ID())
+
+	s.Start()
+
+	select {}
+}
+
+func notify() {
+	notifize.Display("Drink Water", "It's time to drink water 🗣️🗣️🗣️", true, "/home/radi8/Code/Golang/Drink-Agua/icon.png")
+	os.Exit(0)
+}
